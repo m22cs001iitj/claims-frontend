cbf688ce8e683bdd2af594744fc3de9f97e3df2d
m22cs001iitj <145360283+m22cs001iitj@users.noreply.github.com>
2024-07-15 11:19:57 +0530

    Delete Documents directory

--- Documents/upgrade_claims_management_system/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://your_username:your_password@localhost:5432/your_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
